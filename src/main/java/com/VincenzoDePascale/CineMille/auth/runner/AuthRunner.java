12fbc49e239f4ac0525becaaba78839da4b32cb2
Vincenzo De Pascale <vincenzodepascala0@gmail.com>
2023-06-26 12:28:33 +0200

    Generazione automatica dell'ADMIN
    all'avvio vengono automaticamente generati i ruoli ed un utente con ruolo admin se non presenti.

--- src/main/java/com/VincenzoDePascale/CineMille/auth/runner/AuthRunner.java
+++ src/main/java/com/VincenzoDePascale/CineMille/auth/runner/AuthRunner.java
@@ -2,6 +2,7 @@ package com.VincenzoDePascale.CineMille.auth.runner;
 
 import com.VincenzoDePascale.CineMille.auth.entity.ERole;
 import com.VincenzoDePascale.CineMille.auth.entity.Role;
+import com.VincenzoDePascale.CineMille.auth.entity.User;
 import com.VincenzoDePascale.CineMille.auth.repository.RoleRepository;
 import com.VincenzoDePascale.CineMille.auth.repository.UserRepository;
 import com.VincenzoDePascale.CineMille.auth.service.AuthService;
@@ -35,7 +36,20 @@ public class AuthRunner implements ApplicationRunner {
 		System.out.println("AuthRunner run...START");
 
 		System.out.println("AuthRunner run...END");
-//		setRoleDefault();
+		
+		if(roleRepository.findAll().size() == 0) {
+            setRoleDefault();
+        }
+		
+		if(userRepository.findAll().size() == 0) {
+			User user = new User();
+			user.setUsername("admin");
+			user.setEmail("admin@exex.com");
+			user.setPassword(passwordEncoder.encode("PW_admin"));
+			Role admin = roleRepository.findById(1l).get();			
+			user.getRoles().add(admin);
+			userRepository.save(user);
+		}
 
 	}
 
